2aed5e7a7777c4e61094999a2d4eea8685a4d0a9
parlaTharuni <tharuniparla@gmail.com>
2023-11-20 17:38:11 +0530

    Add project files.

---
+++ UsingControllersDemo1/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UsingControllersDemo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Error() => throw new NotImplementedException();
+
+        public ActionResult Info(String t)
+        {
+            ViewData["Message"] = "You have requested info for " + t;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult StudentDetails(String t)
+        {
+            StringBuilder sb = new StringBuilder("<b>Student details entered:</b><br>");
+            sb.Append("Name: " + Request.Form["name"].ToString() + "<Br>");
+            sb.Append("Address: " + Request.Form["address"].ToString() + "<Br>");
+            sb.Append("Subject: " + Request.Form["subject"].ToString() + "<Br>");
+
+            ViewData["message"] = sb.ToString();
+            return View();
+        }
+
+    }
+
+    internal class ErrorViewModel
+    {
+        public object RequestId { get; internal set; }
+    }
+}
+
